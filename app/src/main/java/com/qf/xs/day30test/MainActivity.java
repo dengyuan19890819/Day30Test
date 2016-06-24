7b77a7aba634e475f4200c0537a067a5d7088b46
dengyuan19890819 <1343064660@qq.com>
2016-06-24 16:31:40 +0800

    Initial commit

---
+++ app/src/main/java/com/qf/xs/day30test/MainActivity.java
@@ -0,0 +1,74 @@
+package com.qf.xs.day30test;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.cor_layout);
+        //如果是21以上版本，显示布局背景为黄色
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP)
+        {
+            coordinatorLayout.setBackgroundColor(Color.YELLOW);
+        }else {
+            coordinatorLayout.setBackgroundColor(Color.GRAY);
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("tab1"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab3"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab4"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab5"));
+        tabLayout.addTab(tabLayout.newTab().setText("tab6"));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
